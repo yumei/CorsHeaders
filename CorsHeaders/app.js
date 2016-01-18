2b3c8ea43fe1d6f628e5c16c2fcd040d1bf0d403
yumei <yu.mei@feisystems.com>
2016-01-18 13:40:32 -0500

    add project file

---
+++ CorsHeaders/app.js
@@ -0,0 +1,20 @@
+﻿(function(angular) {
+    'use strict';
+
+    angular.module('plunker', [])
+        .controller('MainCtrl', [
+            '$scope', 'ravenHttpApiService',
+            function($scope, ravenHttpApiService) {
+                $scope.name = 'World';
+                $scope.getClient = function() {
+                    ravenHttpApiService.get('clients/1')
+                        .then(function(response) {
+                            $scope.data = response.data;
+                            $scope.headers = response.headers();
+                            console.log($scope.headers);
+                        });
+
+                };
+            }
+        ]);
+}(angular));
---
+++ CorsHeaders/ravenHttpApi.service.js
@@ -0,0 +1,53 @@
+﻿
+(function(module) {
+    'use strict';
+
+    var ravenHttpApiService = function ($http) {
+
+        var baseUrl = 'http://localhost:9029/Databases/myDB/';
+        var service = {
+            findAll: findAll,
+            get: get,
+            put: put,
+            indexQuery: indexQuery
+        };
+
+        return service;
+
+        function findAll(collectionName, httpParameters) {
+            return $http(angular.extend({
+                url: baseUrl + 'indexes/dynamic/' + collectionName,
+                method: 'JSONP'
+            }, httpParameters));
+        }
+
+        function get(id, httpParameters) {
+            return $http(angular.extend({
+                url: baseUrl + 'docs/' + id + '',
+                method: 'GET',
+                headers: {
+                    /*'Access-Control-Expose-Headers': 'ETag',
+                    'Access-Control-Allow-Headers': 'Access-Control-Expose-Headers'*/
+                }
+            }, httpParameters));
+        }
+
+        function put(document, documentId, metatdata, httpParameters) {
+            return $http(angular.extend({
+                url: baseUrl + 'docs/' + escape(documentId) + '',
+                method: 'PUT',
+                headers: metatdata,
+                data: JSON.stringify(document)
+            }, httpParameters));
+        }
+
+        function indexQuery(indexName, query, httpParameters) {
+            return $http(angular.extend({
+                url: baseUrl + 'indexes/' + indexName + '?&query=' + query,
+                method: 'GET'
+            }, httpParameters));
+        }
+    };
+
+    module.service('ravenHttpApiService', ['$http', ravenHttpApiService]);
+}(angular.module('plunker')));
